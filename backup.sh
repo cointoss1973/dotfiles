6ec91238a3c0ceb4d45caa7d423f6c9dfbc12d97
Takayuki KONDO <tkondou@gmail.com>
2012-03-05 22:09:45 +0900

    backup.sh : change comment and function name

--- backup.sh
+++ backup.sh
@@ -2,20 +2,20 @@
 
 ARCH=$(uname -s)-$(uname -m)
 
-function backup_dotfiles
+function copy_dotfiles
 {
 	DOT_FILES=$(ls -a dot/.??*)   # at least 3words...
  	for dst in ${DOT_FILES[@]}
 	do
-	 	src=${dst##*/}
-     	cp -p $HOME/$src $HOME/dotfiles/$dst
-    	echo $HOME/$src "->" $HOME/dotfiles/$dst 
+		src=${dst##*/}
+		cp -p $HOME/$src $HOME/dotfiles/$dst
+		echo $HOME/$src "->" $HOME/dotfiles/$dst 
 	done
 }
 
 if [[ $ARCH = MINGW* ]]; then
-	# Windows 
-	backup_dotfiles
+	# Oh... No support symblic link system
+	copy_dotfiles
 else
 	echo No Need. Maybe symbolic link support system.
 fi
