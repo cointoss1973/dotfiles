b76f3ecffe42aa5266df0b256ecc216b837f1f38
Takayuki KONDO <tkondou@gmail.com>
2012-09-11 09:04:12 +0900

    support any dotfiles

--- backup.sh
+++ backup.sh
@@ -4,19 +4,18 @@ ARCH=$(uname -s)-$(uname -m)
 
 function copy_dotfiles
 {
-	DOT_FILES=$(ls -a dot/.??*)   # at least 3words...
- 	for dst in ${DOT_FILES[@]}
-	do
-		src=${dst##*/}
-		cp -p $HOME/$src $HOME/dotfiles/$dst
-		echo $HOME/$src "->" $HOME/dotfiles/$dst 
-	done
+    for dst in `find dot -name .\*`
+    do
+	src=${dst##*/}
+	cp -p $HOME/$src $HOME/dotfiles/$dst
+	echo $HOME/$src "->" $HOME/dotfiles/$dst 
+    done
 }
 
-if [[ $ARCH = MINGW* ]]; then
-	# Oh... No support symblic link system
-	copy_dotfiles
+if [[ $ARCH = MINGW* ]]; then	
+    # Oh... No support symblic link system
+    copy_dotfiles
 else
-	echo No Need. Maybe symbolic link support system.
+    echo No Need. Maybe symbolic link support system.
 fi
 
--- diff.sh
+++ diff.sh
@@ -4,18 +4,18 @@ ARCH=$(uname -s)-$(uname -m)
 
 function diff_dotfiles
 {
- 	for dst in `find dot -name .\*`
-	do
-		src=${dst##*/}
-		echo === $src ===
-		diff $HOME/$src $HOME/dotfiles/$dst
-	done
+    for dst in `find dot -name .\*`
+    do
+	src=${dst##*/}
+	echo === $src ===
+	diff $HOME/$src $HOME/dotfiles/$dst
+    done
 }
 
 if [[ $ARCH = MINGW* ]]; then
-	# Oh... No support symblic link system
-	diff_dotfiles
+    # Oh... No support symblic link system
+    diff_dotfiles
 else
-	echo No Need. Maybe symbolic link support system.
+    echo No Need. Maybe symbolic link support system.
 fi
 
